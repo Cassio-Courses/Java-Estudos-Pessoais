879f42929954836d7d8fe9b29ad35eb9c718b55c
Cassio <cassio8186@gmail.com>
2019-08-07 17:34:38 -0300

    3

--- Estudos Pessoais 2/src/estudos/pessoais/pkg2/EstudosPessoais2.java
+++ Estudos Pessoais 2/src/estudos/pessoais/pkg2/EstudosPessoais2.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package estudos.pessoais.pkg2;
-
-/**
- *
- * @author Cassio
- */
 public class EstudosPessoais2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
+
+}
+class PersonTest{
+    public void ReturnHelloWorld(){
+        
+    }
 }
