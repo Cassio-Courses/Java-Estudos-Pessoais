16085908fd8ee717462f79247d570dc7cedf0d5e
Cassio <cassio8186@gmail.com>
2019-08-07 11:55:15 -0300

    3,

--- Estudos Pessoais/src/estudos/pessoais/EstudosPessoais.java
+++ Estudos Pessoais/src/estudos/pessoais/EstudosPessoais.java
@@ -1,7 +1,19 @@
 package estudos.pessoais;
+
+import java.util.Scanner;
+
 public class EstudosPessoais {
     public static void main(String[] args) {
-       
+        Scanner entrada = new Scanner(System.in);
+        float num1, num2, result;
+        System.out.println("Digite um numero");
+        num1 = entrada.nextFloat();
+        System.out.println("Digite outro numero");
+        num2 = entrada.nextFloat();
     }
     
 }
+class Funcoes{
+   
+    }
+
