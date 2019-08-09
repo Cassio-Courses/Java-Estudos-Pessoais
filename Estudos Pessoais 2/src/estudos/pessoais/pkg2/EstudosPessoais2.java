58b87fceb44950da7a4619f71c1c14c52519ec02
Cassio <cassio8186@gmail.com>
2019-08-09 10:54:02 -0300

    Update EstudosPessoais2.java

--- Estudos Pessoais 2/src/estudos/pessoais/pkg2/EstudosPessoais2.java
+++ Estudos Pessoais 2/src/estudos/pessoais/pkg2/EstudosPessoais2.java
@@ -3,12 +3,15 @@ public class EstudosPessoais2 {
 
     public static void main(String[] args) {
         System.out.println(PersonTest.helloWorld());
+        PersonTest Cassio = new PersonTest("Cassio", "seila");
+        System.out.println(Cassio);
     }
 
 }
 class PersonTest{
-    private PersonTest Name;
-    public PersonTest(String Name, String Test){
+    private Name PersonName;
+    public PersonTest(Name PersonName, String Test){
+        this.PersonName = PersonName;
     
 }
     public static String helloWorld(){
