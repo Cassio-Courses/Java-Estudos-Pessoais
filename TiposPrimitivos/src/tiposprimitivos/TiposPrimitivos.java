e8f0d371f89e1aff04afb092aac24b3672031786
Cassio <cassio8186@gmail.com>
2019-08-10 11:17:06 -0300

    Soma

--- TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -1,11 +1,27 @@
 package tiposprimitivos;
+
+import java.util.Scanner;
+
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        float nota = 8.5f;
-        String nome = "Cassin";
+        float nota;
+        String nome;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Escreva o nome");
+        nome = entrada.nextLine();
+        System.out.println("Escreva a nota");
+        nota = entrada.nextFloat();
+        //String valor = Float.toString(nota);
         System.out.println("A nota de " + nome + " é: " + nota);
         System.out.printf("Sua nota de %s é %.1f \n", nome, nota);
-        System.out.format("Sua nota de %s é %.1f \n", nome, nota);
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(valor);
+        
+        
+        
+        
         
     }
     
