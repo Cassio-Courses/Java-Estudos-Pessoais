ba0e9de32eaa76a74a8c8db6150a844d8d6725d0
Cassio <cassio8186@gmail.com>
2019-08-10 10:45:28 -0300

    a

---
+++ TiposPrimitivos/src/tiposprimitivos/TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package tiposprimitivos;
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        float nota = 8.5f;
+        String nome = "Cassin";
+        System.out.println("A nota de " + nome + " é: " + nota);
+        System.out.printf("Sua nota de %s é %.1f \n", nome, nota);
+        System.out.format("Sua nota de %s é %.1f \n", nome, nota);
+        
+    }
+    
+}
