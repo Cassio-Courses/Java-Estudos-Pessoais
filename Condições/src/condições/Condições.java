d5a55afdc32378981031ddb6b1df157e052d3c51
Cassio <cassio8186@gmail.comgit config --listgit config --global user.email cassio8186@gmail.com>
2019-08-11 19:10:12 -0300

    ,j

--- Condições/src/condições/Condições.java
+++ Condições/src/condições/Condições.java
@@ -1,23 +1,40 @@
 package condições;
 
 import java.util.Scanner;
+import java.time.Year;
 
 public class Condições {
-    public static Scanner entrada = new Scanner(System.in); 
+
+    public static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
-        primeira();
-        }
-        public static void primeira() {
+        //primeira();
+        segunda();
+    }
+
+    public static void primeira() {
         System.out.println("Insira a nota 1");
         float n1 = entrada.nextFloat();
         System.out.println("Insira a nota 2");
         float n2 = entrada.nextFloat();
         float media = (n1 + n2) / 2f;
         System.out.println(media);
-        if(media >= 9){
+        if (media >= 9) {
             System.out.println("Parabéns");
         }
+    }
+
+    public static void segunda() {
+        System.out.println("Insira a dada de nascimento");
+        int dataNascimento = entrada.nextInt();
+        int anoAtual = Integer.parseInt(Year.now().toString());
+        int idade = anoAtual - dataNascimento;
+        System.out.println("Sua idade é " + idade);
+        if (idade >= 18){
+            System.out.println("Maior");
+        }
+        else{
+            System.out.println("Menor");
         }
     }
-    
-
+}
