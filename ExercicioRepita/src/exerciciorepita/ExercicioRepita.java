db5257a37adf1c2bb9bbcedb5efa44f44f547fdf
Cassio <cassio8186@gmail.com>
2019-08-12 08:51:09 -0300

    joptionpane

--- ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
+++ ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -1,17 +1,22 @@
 package exerciciorepita;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 public class ExercicioRepita {
     public static void main(String[] args) {
-        int n, s = 0;
+        int n, s = 0, totVal = 0, totPar = 0, totImpar = 0, acimaCem = 0, mediaVal = 0;
+        
         do{
         n = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe um numero"));
         s  += n;
         
         
         }while(n != 0);
-        JOptionPane.showMessageDialog(null, "A soma dos valores é: " + s,"Soma Total" ,1);
-        https://youtu.be/sUSnoHiTZ8E?list=1&t=775 continuação exercicio
+        JOptionPane.showMessageDialog(null, "<html>A soma dos valores é: " + s + 
+                "<hr>" +
+                " <br> Total de Valores: " + totVal + 
+                " <br> Total de Pares: " + totPar +
+                " <br> Total de Ímpares: " +  totImpar + 
+                " <br> Acima de 100: " + acimaCem +
+                " <br> Média de Valores: " + mediaVal + "</html>" ,"Soma Total" ,1);
         
     }
     
