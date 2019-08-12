3cf9ed537ec2bf043edf69c4c8f79628fec763ec
Cassio <cassio8186@gmail.com>
2019-08-12 09:57:59 -0300

    Fim exercicio

--- ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
+++ ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -2,14 +2,28 @@ package exerciciorepita;
 import javax.swing.JOptionPane;
 public class ExercicioRepita {
     public static void main(String[] args) {
-        int n, s = 0, totVal = 0, totPar = 0, totImpar = 0, acimaCem = 0, mediaVal = 0;
+        int n, s = 0, totVal = 0, totPar = 0, totImpar = 0, acimaCem = 0;
+        float mediaVal = 0;
         
         do{
         n = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe um numero"));
+        if (n != 0){
         s  += n;
-        
-        
+        totVal++;
+        //TotparImpar
+        if (n % 2 == 0){
+            totPar++;
+        }
+        else{
+            totImpar++;
+        }
+        if (n > 100){
+            acimaCem++;
+        }
+        mediaVal +=  (float) n;
+        }
         }while(n != 0);
+        mediaVal /= (float) totVal;
         JOptionPane.showMessageDialog(null, "<html>A soma dos valores é: " + s + 
                 "<hr>" +
                 " <br> Total de Valores: " + totVal + 
@@ -17,7 +31,8 @@ public class ExercicioRepita {
                 " <br> Total de Ímpares: " +  totImpar + 
                 " <br> Acima de 100: " + acimaCem +
                 " <br> Média de Valores: " + mediaVal + "</html>" ,"Soma Total" ,1);
-        
+                System.out.println(mediaVal);
+                
     }
     
 }
