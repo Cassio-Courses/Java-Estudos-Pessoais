0c342afedab2adfd75b356460c68588d8b066cd8
Cassio <cassio8186@gmail.com>
2019-08-12 11:55:33 -0300

    Vetores && RepetiçãoFor

---
+++ RepetiçãoFor/src/repetiçãofor/RepetiçãoFor.java
@@ -0,0 +1,16 @@
+package repetiçãofor;
+public class RepetiçãoFor {
+    
+    public static void main(String[] args) {
+        int cc;
+        for ( cc = 0; cc <= 4; cc++){
+            System.out.println(cc);
+        }
+        for (cc = 0; cc <= 100; cc += 10){
+            System.out.println(cc);
+            
+        }
+        System.out.println(cc);
+    }
+    
+}
---
+++ Vetores/src/vetores/Vetores.java
@@ -0,0 +1,16 @@
+package vetores;
+
+import java.util.Arrays;
+
+public class Vetores {
+    public static void main(String[] args) {
+        int cc;
+        int n[] = new int [4];
+        for (cc = 0; cc<=3;++cc){
+            n[cc]=cc;
+            System.out.println(n[cc]);
+        }
+        
+    }
+    
+}
