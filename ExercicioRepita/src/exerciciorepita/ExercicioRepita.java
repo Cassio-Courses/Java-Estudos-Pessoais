52e4fa007f34a733a71c1c7a1f4c0802f99da62e
Cassio <cassio8186@gmail.comgit config --listgit config --global user.email cassio8186@gmail.com>
2019-08-11 23:41:10 -0300

    Update ExercicioRepita.java

--- ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
+++ ExercicioRepita/src/exerciciorepita/ExercicioRepita.java
@@ -10,7 +10,9 @@ public class ExercicioRepita {
         
         
         }while(n != 0);
-        JOptionPane.showMessageDialog(null, "A soma dos valores é: " + s,"Soma Total" , 3);
+        JOptionPane.showMessageDialog(null, "A soma dos valores é: " + s,"Soma Total" ,1);
+        https://youtu.be/sUSnoHiTZ8E?list=1&t=775 continuação exercicio
+        
     }
     
 }
