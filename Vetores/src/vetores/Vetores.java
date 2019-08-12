5e8feba3949cc13955782a003ede2d4c35613dfc
Cassio8186 <cassio8186@gmail.com>
2019-08-12 20:26:04 -0300

    vetores

--- Vetores/src/vetores/Vetores.java
+++ Vetores/src/vetores/Vetores.java
@@ -1,16 +1,13 @@
 package vetores;
 
-import java.util.Arrays;
-
 public class Vetores {
     public static void main(String[] args) {
-        int cc;
-        int n[] = new int [4];
-        for (cc = 0; cc<=3;++cc){
-            n[cc]=cc;
-            System.out.println(n[cc]);
+        int cc = 0;
+        int n[] = {5,4,7,2,9,7,5,3,2,5,7,9,6,54,34,3,4,5,6,6,2,45,10,2,1,204,5};
+        while (cc < n.length){
+            System.out.print(n[cc++] + ", ");
         }
-        
+    
     }
     
 }
