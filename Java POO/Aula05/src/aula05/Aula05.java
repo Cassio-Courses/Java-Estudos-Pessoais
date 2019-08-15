9e53bd4fc3932034b566e11cd5bb73ea789e507f
Cassio <cassio8186@gmail.comgit config --listgit config --global user.email cassio8186@gmail.com>
2019-08-15 16:01:15 -0300

    top

--- Java POO/Aula05/src/aula05/Aula05.java
+++ Java POO/Aula05/src/aula05/Aula05.java
@@ -2,6 +2,7 @@ package aula05;
 public class Aula05 {
     public static void main(String[] args) {
         ContaBanco c1 = new ContaBanco(1, "Cassio");
+        ContaBanco c2 = new ContaBanco(2, "Jubileu");
         System.out.println(c1.fecharConta());
         System.out.println(c1.abrirConta("CC"));
         System.out.println(c1.depositar(500));
@@ -13,6 +14,9 @@ public class Aula05 {
         System.out.println(c1.fecharConta());
         System.out.println(c1.status());
         System.out.println(c1.pagarMensal());
+        System.out.println(c2.abrirConta("CP"));
+        System.out.println(c2.status());
+        
     }
     
 }
--- Java POO/Aula05/src/aula05/ContaBanco.java
+++ Java POO/Aula05/src/aula05/ContaBanco.java
@@ -15,6 +15,80 @@ public class ContaBanco {
         this.status = false;
     }
 
+    //Methods below
+    public String abrirConta(String tipo) {
+
+        setStatus(true);
+        setTipo(tipo);
+        if ("CC".equals(tipo)) {
+            setSaldo(50);
+            return "Conta Corrente aberta com saldo de 50 reais";
+        } else {
+            setSaldo(150);
+            return "Conta Poupança aberta com saldo de 150 reais";
+        }
+    }
+
+    public String fecharConta() {
+        /**
+         * Regras de negócio pra fechar conta não pode ter dinheiro, nem débito.
+         */
+        if (isStatus() == false) {
+            return "Não foi possível fechar a conta pois ela não está aberta";
+        } else if (getSaldo() > 0) {
+            return "Não foi possível fechar conta, pois possui um saldo de: " + getSaldo();
+        } else if (getSaldo() < 0) {
+            return "Não foi possível fechar conta, pois possui um saldo negativo de: " + getSaldo();
+        } else {
+            setStatus(false);
+            return "Conta foi fechada com sucesso";
+        }
+    }
+
+    /**
+     * sacar() precisa estar aberta e precisa ter saldo para saque.
+     * pagarMensal() ao ser chamado, cliente que tem conta corrente pagará 12
+     * reais
+     */
+    public String depositar(float valor) {
+        if (isStatus()) {
+            setSaldo(getSaldo() + valor);
+            return valor + " Foi adicionado a sua conta";
+        } else {
+            return "Sua conta não está aberta";
+        }
+    }
+
+    public String sacar(float valor) {
+        if (isStatus() == false) {
+            return "Sua conta não está aberta";
+        }
+        if (valor > getSaldo()) {
+            return "Você tentou sacar o valor " + valor + "e acima do saldo atual que é: " + this.saldo;
+        } else {
+            setSaldo(getSaldo() - valor);
+            return "Valor " + valor + " foi sacado com sucesso";
+        }
+    }
+
+    /**
+     * pagarMensal() ao ser chamado, cliente que tem conta corrente pagará 12
+     * reais cliente que tem poupança pagará 20 reais todos métodos publicos.
+     */
+    public String pagarMensal() {
+        if ("CC".equals(getTipo())) {
+            this.sacar(12f);
+            return "Foi pago valor de: 12 reais";
+        } else {
+            this.sacar(20f);
+            return "Foi pago valor de: 20 reais";
+        }
+    }
+
+    public String status() {
+        return "ContaBanco: " + "Numero da conta: " + numConta + ", Tipo: " + tipo + ", Dono: " + dono + ", Saldo: " + saldo + ", Status: " + status + '.';
+    }
+
     //Getters and Setters begin
     public int getNumConta() {
         return numConta;
@@ -57,75 +131,4 @@ public class ContaBanco {
     }
 
     //Getters and setters end
-    //Methods below
-    public String abrirConta(String tipo) {
-
-        setStatus(true);
-        setTipo(tipo);
-        if ("CC".equals(tipo)) {
-            setSaldo(50);
-            return "Conta Corrente aberta com saldo de 50 reais";
-        } else {
-            setSaldo(150);
-            return "Conta Poupança aberta com saldo de 150 reais";
-        }
-    }
-
-    public String fecharConta() {
-        /**
-         * Regras de negócio pra fechar conta não pode ter dinheiro, nem débito.
-         */
-        if (isStatus() == false) {
-            return "Não foi possível fechar a conta pois ela não está aberta";
-        }
-        if (getSaldo() != 0) {
-            return "Não foi possível fechar conta, pois possui um saldo de: " + getSaldo();
-        }
-        setStatus(false);
-        return "Conta foi fechada com sucesso";
-    }
-
-    /**
-     * sacar() precisa estar aberta e precisa ter saldo para saque.
-     * pagarMensal() ao ser chamado, cliente que tem conta corrente pagará 12
-     * reais
-     */
-    public String depositar(float valor) {
-        if (isStatus()) {
-            setSaldo(getSaldo() + valor);
-            return valor + " Foi adicionado a sua conta";
-        } else {
-            return "Sua conta não está aberta";
-        }
-    }
-
-    public String sacar(float valor) {
-        if (isStatus() == false) {
-            return "Sua conta não está aberta";
-        }
-        if (valor > getSaldo()) {
-            return "Você tentou sacar o valor " + valor + "e acima do saldo atual que é: " + this.saldo;
-        } else {
-            setSaldo(getSaldo() - valor);
-            return "Valor " + valor + " foi sacado com sucesso";
-        }
-    }
-
-    /**
-     * pagarMensal() ao ser chamado, cliente que tem conta corrente pagará 12
-     * reais cliente que tem poupança pagará 20 reais todos métodos publicos.
-     */
-    public String pagarMensal() {
-        if ("CC".equals(getTipo())) {
-            this.sacar(12f);
-            return "Foi pago valor de: 12 reais";
-        } else {
-            this.sacar(20f);
-            return "Foi pago valor de: 20 reais";
-        }
-    }
-    public String status() {
-        return "ContaBanco: " + "Numero da conta: " + numConta + ", Tipo: " + tipo + ", Dono: " + dono + ", Saldo: " + saldo + ", Status: " + status + '.';
-    }
-
 }
