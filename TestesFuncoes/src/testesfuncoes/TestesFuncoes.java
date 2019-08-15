1469ebd100a61c9e084b79e95541e2bfd476fb3a
Cassio <cassio8186@gmail.comgit config --listgit config --global user.email cassio8186@gmail.com>
2019-08-14 22:53:15 -0300

    Fim

---
+++ TestesFuncoes/src/testesfuncoes/TestesFuncoes.java
@@ -0,0 +1,7 @@
+package testesfuncoes;
+public class TestesFuncoes {
+    public static void main(String[] args) {
+        System.out.println("Contagem será iniciada: " + (Operacoes.contador(5, 10)));
+    }
+    
+}
