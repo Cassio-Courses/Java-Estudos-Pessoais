862d4e8e7dcac207fe09568bcb481805342472f1
Cassio <cassio8186@gmail.com>
2019-08-17 18:45:49 -0300

    Formatação

--- Aula09/src/aula09/Aula09.java
+++ Aula09/src/aula09/Aula09.java
@@ -1,5 +1,7 @@
 package aula09;
+
 public class Aula09 {
+
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa("Cassio", 21, 'M');
         Livro l1 = new Livro("A volta dos que não foram", "Juninho", 10, p1);
@@ -8,15 +10,14 @@ public class Aula09 {
         l1.abrir();
         l1.fechar();
         l1.fechar();
-        for(int c = 0; c <= 12; c++){
-            if(c == 1){
+        for (int c = 0; c <= 12; c++) {
+            if (c == 1) {
                 l1.abrir();
             }
             l1.voltarPag();
         }
         l1.folhear();
-        
+
     }
 
-    
 }
