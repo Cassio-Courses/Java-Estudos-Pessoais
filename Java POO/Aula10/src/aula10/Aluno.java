198015ee10e23845a62fdf6d20adb1da9148265c
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 19:35:16 -0300

    Aluno

--- Java POO/Aula10/src/aula10/Aluno.java
+++ Java POO/Aula10/src/aula10/Aluno.java
@@ -1,7 +1,38 @@
 package aula10;
-public class Aluno extends Pessoa{
-    public static void teste(){
-            System.out.println("teste");
-}
-    
+
+public class Aluno extends Pessoa {
+
+    private int matr;
+    private String curso;
+
+    //Métodos especiais
+    public void cancelarMatr() {
+        setMatr(0);
+    }
+
+    //Método construtor
+    public Aluno(int matr, String curso, String nome, int idade, char sexo) {
+        super(nome, idade, sexo);
+        this.matr = matr;
+        this.curso = curso;
+
+    }
+    //Getters and Setters
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
 }
