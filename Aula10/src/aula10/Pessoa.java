01169814451c03e391d31eaa807ff574cd8cf356
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 18:49:07 -0300

    Constructors getters setters e métodos especiais

--- Aula10/src/aula10/Pessoa.java
+++ Aula10/src/aula10/Pessoa.java
@@ -1,4 +1,46 @@
 package aula10;
 public class Pessoa {
+    private String nome;
+    private int idade;
+    private char sexo;
+    //Métodos especiais
+    public void fazerAniv(){
+        setIdade(getIdade() + 1);
+    }
+    //Constructor
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, int idade, char sexo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+    }
+    //Getters and setters
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
     
 }
