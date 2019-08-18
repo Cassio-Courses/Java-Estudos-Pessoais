1b5080f8d026b13f4d14fad343783051d3a29113
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 19:37:25 -0300

    Update Professor.java

--- Java POO/Aula10/src/aula10/Professor.java
+++ Java POO/Aula10/src/aula10/Professor.java
@@ -1,4 +1,18 @@
 package aula10;
-public class Professor {
-    
+
+public class Professor extends Pessoa {
+
+    private String setor;
+    private boolean trabalhando;
+
+    public void mudarTrabalho() {
+
+    }
+
+    public Professor(String setor, boolean trabalhando, String nome, int idade, char sexo) {
+        super(nome, idade, sexo);
+        this.setor = setor;
+        this.trabalhando = trabalhando;
+    }
+
 }
