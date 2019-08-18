2c2aef6f0fbd9d2a83d605451dfeed6b95b8abba
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 19:42:13 -0300

    Update Professor.java

--- Java POO/Aula10/src/aula10/Professor.java
+++ Java POO/Aula10/src/aula10/Professor.java
@@ -1,5 +1,34 @@
 package aula10;
 
 public class Professor extends Pessoa {
-    
+
+    private String especialidade;
+    private float salario;
+
+    public void receberAum() {
+
+    }
+
+    public Professor(String especialidade, float salario, String nome, int idade, char sexo) {
+        super(nome, idade, sexo);
+        this.especialidade = especialidade;
+        this.salario = salario;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
 }
