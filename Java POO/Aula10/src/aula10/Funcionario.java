5051d98f0fc3376a1c7da284178219734fae82bc
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 19:41:01 -0300

    funcionario

--- Java POO/Aula10/src/aula10/Funcionario.java
+++ Java POO/Aula10/src/aula10/Funcionario.java
@@ -1,4 +1,35 @@
 package aula10;
-public class Funcionario {
+
+public class Funcionario extends Pessoa {
+
+    private String setor;
+    private boolean trabalhando;
+
+    public void mudarTrabalho() {
+
+    }
+
+    public Funcionario(String setor, boolean trabalhando, String nome, int idade, char sexo) {
+        super(nome, idade, sexo);
+        this.setor = setor;
+        this.trabalhando = trabalhando;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public boolean isTrabalhando() {
+        return trabalhando;
+    }
+
+    public void setTrabalhando(boolean trabalhando) {
+        this.trabalhando = trabalhando;
+    }
     
+
 }
--- Java POO/Aula10/src/aula10/Professor.java
+++ Java POO/Aula10/src/aula10/Professor.java
@@ -1,18 +1,5 @@
 package aula10;
 
 public class Professor extends Pessoa {
-
-    private String setor;
-    private boolean trabalhando;
-
-    public void mudarTrabalho() {
-
-    }
-
-    public Professor(String setor, boolean trabalhando, String nome, int idade, char sexo) {
-        super(nome, idade, sexo);
-        this.setor = setor;
-        this.trabalhando = trabalhando;
-    }
-
+    
 }
