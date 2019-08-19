f0e6c45cff4ae86eb2b5e5bd4f02ed9062cbde31
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 21:23:41 -0300

    Create Bolsista.java

---
+++ Java POO/Aula11/src/aula11/Bolsista.java
@@ -0,0 +1,8 @@
+package aula11;
+public class Bolsista extends Aluno{
+    
+    public Bolsista(String nome, int idade, char sexo, int matr, String curso) {
+        super(nome, idade, sexo, matr, curso);
+    }
+    
+}
