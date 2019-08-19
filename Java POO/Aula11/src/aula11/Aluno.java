635e9a4b14a0577fc1071721d96dbed0cc133ed7
Cassio Lopes <cassio8186@gmail.com>
2019-08-18 21:15:31 -0300

    agora vai

---
+++ Java POO/Aula11/src/aula11/Aluno.java
@@ -0,0 +1,47 @@
+package aula11;
+
+public class Aluno extends Pessoa {
+
+    private int matr;
+    private String curso;
+
+    //Métodos especiais
+
+    /**
+     * Seta matricula 0
+     */
+    public void cancelarMatr() {
+        setMatr(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "matr=" + matr + ", curso=" + curso + '}';
+    }
+
+    //Método construtor
+    public Aluno(String nome, int idade, char sexo, int matr, String curso) {
+        super(nome, idade, sexo);
+        this.matr = matr;
+        this.curso = curso;
+
+    }
+    //Getters and Setters
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+}
