2980eba962d55195d9ddd75b0f5394d5e80b90e8
Cassio Lopes <cassio8186@gmail.com>
2019-08-22 19:20:04 -0300

    Refatorado Animal Abstrato para mamifero, criação classe mamifero

---
+++ Java POO/Aula12/src/aula12/Animal.java
@@ -0,0 +1,8 @@
+package aula12;
+public abstract class Animal {
+    protected double peso;
+    protected int idade, membros;
+    public abstract void locomover();
+    public abstract void alimentar();
+    public abstract void emitirsom();
+}
--- Java POO/Aula12/src/aula12/Mamifero.java
+++ Java POO/Aula12/src/aula12/Mamifero.java
@@ -1,8 +1,4 @@
 package aula12;
-public abstract class Mamifero {
-    protected double peso;
-    protected int idade, membros;
-    public abstract void locomover();
-    public abstract void alimentar();
-    public abstract void emitirsom();
+public class Mamifero {
+    
 }
