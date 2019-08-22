4e5faab8ad5e6f0cd6348f052e8d6cd9a1b1ef6a
Cassio Lopes <cassio8186@gmail.com>
2019-08-22 19:37:22 -0300

    Create Ave.java

---
+++ Java POO/Aula12/src/aula12/Ave.java
@@ -0,0 +1,25 @@
+package aula12;
+
+public class Ave extends Animal {
+
+    private String corPena;
+
+    @Override
+    public void locomover() {
+        System.out.println("Voando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo frutas");
+    }
+
+    @Override
+    public void emitirsom() {
+        System.out.println("Som da ave");
+    }
+
+    public void fazerNinho() {
+        System.out.println("Construiu um ninho");
+    }
+}
