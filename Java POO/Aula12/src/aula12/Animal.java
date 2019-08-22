4a9d620379e535d772f9b37cc56cd3d8a685fb56
Cassio Lopes <cassio8186@gmail.com>
2019-08-22 19:29:18 -0300

    Inserido construtor animal (Tentativa)
    Pode estar errado

--- Java POO/Aula12/src/aula12/Animal.java
+++ Java POO/Aula12/src/aula12/Animal.java
@@ -1,8 +1,14 @@
 package aula12;
+
 public abstract class Animal {
+
     protected double peso;
     protected int idade, membros;
+
     public abstract void locomover();
+
     public abstract void alimentar();
+
     public abstract void emitirsom();
+
 }
--- Java POO/Aula12/src/aula12/Mamifero.java
+++ Java POO/Aula12/src/aula12/Mamifero.java
@@ -1,4 +1,29 @@
 package aula12;
-public class Mamifero {
-    
+
+public class Mamifero extends Animal {
+
+    private String corPelo;
+
+    public Mamifero(int idade, int membros, double peso, String corPelo) {
+        this.corPelo = corPelo;
+        this.peso = peso;
+        this.idade = idade;
+        this.membros = membros;
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Se alimentando");
+    }
+
+    @Override
+    public void emitirsom() {
+        System.out.println("Emitindo som");
+    }
+
 }
