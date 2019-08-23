eefb29b0393bd3784fcdac9ac89fa9f3b0f8c901
Cassio Lopes <cassio8186@gmail.com>
2019-08-22 21:38:35 -0300

    Adicionado visualizacao

---
+++ Java POO/Aula15/src/aula15/Visualizacao.java
@@ -0,0 +1,36 @@
+package aula15;
+
+public class Visualizacao {
+
+    private Gafanhoto espectador;
+    private Video filme;
+
+    public Visualizacao(Gafanhoto espectador, Video filme) {
+        this.espectador = espectador;
+        this.filme = filme;
+        this.espectador.setTotAssistindo(this.espectador.getTotAssistindo() + 1);
+        this.filme.setVisualizacoes(this.filme.getVisualizacoes() + 1);
+    }
+
+    public Gafanhoto getEspectador() {
+        return espectador;
+    }
+
+    public void setEspectador(Gafanhoto espectador) {
+        this.espectador = espectador;
+    }
+
+    public Video getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Video filme) {
+        this.filme = filme;
+    }
+
+    @Override
+    public String toString() {
+        return "Visualizacao{" + "espectador=" + espectador + ",\n filme=" + filme + '}';
+    }
+
+}
